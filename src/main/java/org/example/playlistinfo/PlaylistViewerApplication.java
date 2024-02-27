033651ba55a86ef54a6dfe22d1d89ae1146dd7c8
oosm032519 <ryooshima0519@gmail.com>
2024-02-27 20:39:25 +0900

    JDKのバージョンを変更

--- src/main/java/org/example/playlistinfo/PlaylistViewerApplication.java
+++ src/main/java/org/example/playlistinfo/PlaylistViewerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class PlaylistInfoApplication {
+public class PlaylistViewerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(PlaylistInfoApplication.class, args);
+        SpringApplication.run(PlaylistViewerApplication.class, args);
     }
 }
